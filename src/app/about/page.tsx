3d0f2a08f2015e25f3884511ebecf52303c2ad2a
semm00 <sofiasemmemily@gmail.com>
2025-09-20 23:16:31 -0300

    structuring and projecting

---
+++ src/app/about/page.tsx
@@ -0,0 +1,23 @@
+/*
+About Page "Portal PM"
+Esta é a página sobre a cidade de Padre Marcos, onde você encontrará informações sobre a cidade, sua história e muito mais.
+
+estrutura da página:
+- Navegação principal (Header)
+- Seção com informações gerais do município (dados demográficos e geográficos
+- Seção com história da cidade
+- Seção com atrações turísticas
+- Footer com informações e links úteis
+- Responsividade para dispositivos móveis
+*/
+
+
+
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
---
+++ src/app/feed/page.tsx
@@ -0,0 +1,21 @@
+/*
+Feed Page "Portal PM"
+Esta é a página de feed do Portal PM, onde você encontrará as últimas atualizações, galeria com fotos, notícias e eventos relacionados à cidade de Padre Marcos.
+
+estrutura da página:
+- Navegação principal (Header)
+- Seção com calendário de eventos
+- Seção com galeria de fotos
+- Formulário para envio de fotos, vídeos e atualizações pelos usuários
+- Footer com informações e links úteis
+- Responsividade para dispositivos móveis
+*/
+
+
+export default function Feed() {
+  return (
+    <div>
+      <h1>Feed Page</h1>
+    </div>
+  );
+}
